224e907f48d438f562f10590b1c9c54ca0b22d75
Lionel Bergeret <lbergeret@gmail.com>
2013-12-27 11:25:26 +0900

    Add CSV output support

--- safecastSummary.py
+++ safecastSummary.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-# Import shapefile informations
-from shapelib import ShapeFile
-import dbflib
-from shapely.geometry import Polygon
-
 # Database
 import pymongo
 
@@ -15,7 +10,11 @@ import time
 import simplejson as json
 from optparse import OptionParser
 
+# -----------------------------------------------------------------------------
+# Load the measurement into local mongodb (TokuMX with fractal tree index)
+# -----------------------------------------------------------------------------
 def loadMeasurements(datafile):
+  print "Loading %s into the database ..." % datafile
   connection = pymongo.Connection()
   db = connection.safecast
   locations = db.locations
@@ -28,6 +27,7 @@ def loadMeasurements(datafile):
 
   # Process data
   count = 0
+  failedCount = 0
   insertList = []
   start = time.time()
   end = time.time()
@@ -41,12 +41,13 @@ def loadMeasurements(datafile):
        item[name] = value.strip()
 
     try:
-      count+=1
-      json = {'loc': {"Longitude" : float(item["Longitude"]), "Latitude" : float(item["Latitude"])},
-            'cpm': float(item["Value"])
-          }
+      count += 1
+      json = {'loc': {"Longitude" : float(item["Longitude"]),
+                      "Latitude" : float(item["Latitude"])},
+              'cpm': float(item["Value"])}
       insertList.append(json)
     except:
+      failedCount += 1
       pass
 
     if len(insertList) > 5000:
@@ -58,12 +59,19 @@ def loadMeasurements(datafile):
       print count, end - start
       start = time.time()
 
+  if len(insertList):
+    locations.insert(insertList)
+  print "%d measurements processed (%d failed)" % (count, failedCount)
+
   print "Creating index ..."
   # locations.create_index([("loc", pymongo.GEOSPHERE)])
   locations.create_index([('loc', pymongo.GEO2D)])
   connection.disconnect()
   print "Done."
 
+# -----------------------------------------------------------------------------
+# Swap the latitude and longitude from the polygon provided from GeoJSON
+# -----------------------------------------------------------------------------
 def swapCoordinates(data):
   new = []
   for d in data:
@@ -97,9 +105,13 @@ if __name__ == '__main__':
   db = connection.safecast
   locations = db.locations
 
+  # Write output to CSV file
+  csvfile = open("summary.csv", "wb")
+  writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+  writer.writerow(["Country name", "Measurements"])
+
   # GeoJSON countries
-  # https://github.com/johan/world.geo.json/blob/master/countries.geo.json
-  # {"type":"Feature","id":"AFG","properties":{"name":"Afghanistan"},"geometry":{"type":"Polygon","coordinates":[[[61.210817,35.650072],[62.230651,35.270664],[62.984662,35.404041],[63.193538,35.857166],[63.982896,36.00795] ... ]}},
+  # from: https://github.com/johan/world.geo.json/blob/master/countries.geo.json
   countries = json.loads(open("countries.geo.json").read())["features"]
   for country in countries:
     totalcount = 0
@@ -107,16 +119,19 @@ if __name__ == '__main__':
     polygonList = country["geometry"]["coordinates"]
     polygonType = country["geometry"]["type"]
 
+    print "Processing %s [%d] ..." % (name, len(polygonList))
     if polygonType == "Polygon":
       polygon = polygonList[0]
-      cursors = locations.find({'loc': {'$within': { "$polygon" : swapCoordinates(polygon) }}})
+      cursors = locations.find({'loc': {'$within': {"$polygon": swapCoordinates(polygon)}}})
       totalcount = cursors.count()
     else:
       for polygon in polygonList:
         if len(polygon) == 1:
           polygon = polygon[0]
-        cursors = locations.find({'loc': {'$within': { "$polygon" : swapCoordinates(polygon) }}})
+        cursors = locations.find({'loc': {'$within': {"$polygon": swapCoordinates(polygon)}}})
         totalcount = totalcount + cursors.count()
 
     if totalcount > 0:
-      print "%s,%d" % (name, totalcount)
+      writer.writerow([name, totalcount])
+
+  csvfile.close()
